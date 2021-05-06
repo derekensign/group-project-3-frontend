1fd945f4c2725b775471f636a7f7c31e96a5bf65
bbrad3 <68521389+bbrad3@users.noreply.github.com>
2021-05-06 16:14:03 -0600

    Cart items show in cart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,7 +1,35 @@
+import axios from "axios"
+import { useContext, useEffect, useState } from "react"
+import { GlobalStore } from '../contexts/GlobalStore'
+
 const Cart = () => {
+    const { userState } = useContext(GlobalStore)
+    const [user, setUser] = userState
+    const [cart, setCart] = useState([])
+
+    const fetchCart = () => {
+        console.log(user.id)
+        axios.get(`${process.env.REACT_APP_BACKEND}/users/cart`, {
+            headers: {
+                Authorization: user.id
+            }
+        })
+            .then(res => {
+                console.log('fetchCart res', res);
+                setCart(res.data.products)
+            })
+            .catch(error => {
+                console.error(error);
+            })
+    }
+
+    useEffect(fetchCart, [user])
+
     return (
         <div>
-            Cart goes here
+            {cart && cart.map(item => (
+                <p>{item.name}</p>
+        ))}
         </div>
     )
 }
