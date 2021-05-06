f3e42928e5bcd28adaa2a2581f541bc72e90faa1
bbrad3 <68521389+bbrad3@users.noreply.github.com>
2021-05-05 21:41:01 -0600

    Created GlobalStore and NavBar component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import { GlobalStore } from './contexts/GlobalStore'
+import { useEffect, useContext } from 'react'
+import NavBar from './components/NavBar';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const { userState, fetchUser } = useContext(GlobalStore)
+    const [user, setUser] = userState
+    
+    let local = localStorage.getItem('userId')
+    useEffect(() => {
+      if (local) {fetchUser()}
+    }, [])
+
+    return (
+      <div className="App">
+        <NavBar />
+      </div>
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom'
+import { Provider } from './contexts/GlobalStore';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <React.StrictMode>
+      <Provider>
+        <App />
+      </Provider>
+    </React.StrictMode>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
